a15ad132d164e613809b3dd7d15714601bf319ea
CongSonClone <congson0710@gmail.com>
2017-12-08 13:37:22 +0700

    add some comments to understand clearly

--- database.js
+++ database.js
@@ -1,30 +1,34 @@
+// mySQL server
 const mysql = require("mysql");
+// Promise for result from callback functions
 const q = require("q");
 
+// config sever
 var _HOST = '127.0.0.1',
     _USER = 'root',
     _PWD = '1412468',
     _DB = 'userinfo';
 
+// load data (exports for using in another file)
 exports.load = function (queryString) {
+    // create connection with server
     const connection = mysql.createConnection({
         host: _HOST,
         user: _USER,
         password: _PWD,
         database: _DB
     })
-
+    // "promise" variable
     var defer = q.defer();
-
+    // connect with server
     connection.connect();
-
+    // start querying, pass result in callback functions
     connection.query(queryString, function (error, results) {
-        // console.log("Data from DB", results);
-        // console.log("error", error);
 
+        // create "promise"
         defer.resolve(results);
     })
-
+    // close a connection
     connection.end();
 
     return defer.promise;
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 const db = require("./database");
 
+// express module
 const app = express();
-
+// morgan module for "middlewares"
 app.use(morgan("dev"))
 
 app.get("/hello", function (req, res) {
+    // tell "db.load" to catch "promise" after excuting "load"
     db.load("SELECT * FROM userinfo.user_profile").then(function (results) {
         console.log("RESULT", results);
     })
