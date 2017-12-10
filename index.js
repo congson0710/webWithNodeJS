ea106e546db06b03a4e5dde7803ec0cac33e472f
CongSonClone <congson0710@gmail.com>
2017-12-10 15:32:20 +0700

    Setup view engine for server (using handlebars)

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const express = require("express");
 const morgan = require("morgan");
 const controler = require("./query");
+const handlebars = require("express-handlebars");
 
 // express module
 const app = express();
 // morgan module for "middlewares"
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
-app.get("/hello", function (req, res) {
-    // tell "db.load" to catch "promise" after excuting "load"
-    controler.getUserByUserID().then(function (rows) {
-        console.log("RESULT:", rows);
-    })
+app.engine(
+  "hbs",
+  handlebars({
+    extname: "hbs",
+    defaultLayout: "main",
+    layoutsDir: "views/layouts/",
+    partialsDir: "views/partials/"
+  })
+);
 
+app.set("view engine", "hbs");
 
+app.get("/hello", function(req, res) {
+  // tell "db.load" to catch "promise" after excuting "load"
+  controler.getAllUser().then(function(arrayUser) {
+    console.log("RESULT:", arrayUser);
+  });
 
-    // db.load("SELECT * FROM userinfo.user_profile").then(function (results) {
-    //     console.log("RESULT", results);
-    // })
-    res.end("HELLO WORLD");
-})
+  // db.load("SELECT * FROM userinfo.user_profile").then(function (results) {
+  //     console.log("RESULT", results);
+  // })
+  res.end("HELLO WORLD");
+});
 app.listen(8000);
